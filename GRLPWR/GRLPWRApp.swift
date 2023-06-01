cb7268c83f90698938b65754ef12942b5fc14610
evelyntheodora <129470436+evelyntheodora@users.noreply.github.com>
2023-06-01 16:37:09 +0700

    Initial Commit

---
+++ GRLPWR/GRLPWRApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRLPWRApp.swift
+//  GRLPWR
+//
+//  Created by Evelyn Theodora Siswanto on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GRLPWRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
