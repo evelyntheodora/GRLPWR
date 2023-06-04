4e88530ab231746eaf59527f8eae6633327412c1
rahmiumarefi <129579986+rahmiumarefi@users.noreply.github.com>
2023-06-04 21:23:53 +0700

    Update All Activities View

--- GRLPWR/View/AllActivityView.swift
+++ GRLPWR/View/AllActivityView.swift
@@ -7,9 +7,80 @@
 
 import SwiftUI
 
+struct SearchBar: UIViewRepresentable {
+    @Binding var searchText: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var searchText: String
+
+        init(searchText: Binding<String>) {
+            _searchText = searchText
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            self.searchText = searchText
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(searchText: $searchText)
+    }
+
+    func makeUIView(context: Context) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: Context) {
+        uiView.text = searchText
+    }
+}
+
 struct AllActivityView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack  {
+            HStack  {
+                Button {
+                    
+                }
+            label: {
+                Image(systemName: "chevron.left")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 15, height: 15)
+                    .padding()
+                }
+           
+                Text ("All Activities")
+                    .padding(.leading, 100)
+        
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+           
+            Image(systemName: "magnifyingglass.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 169, height: 190)
+               
+            ZStack {
+                       SearchBar(searchText: $searchText)
+                       Text("Find Your Activity \(searchText)")
+                           .foregroundColor(.gray)
+                           .padding(.trailing, 170)
+                
+                
+                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                            .foregroundColor(.gray)
+                            .padding(.leading, 320)
+                   }
+            
+            Spacer()
+            
+        
+        }
     }
 }
 
