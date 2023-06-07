ff8b3ce52819939c286ece9ed644a80c842c60b6
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-06-07 23:08:46 +0700

    all activity view sementara

--- GRLPWR/View/AllActivityView.swift
+++ GRLPWR/View/AllActivityView.swift
@@ -37,51 +37,111 @@ struct SearchBar: UIViewRepresentable {
     }
 }
 
-struct AllActivityView: View {
+struct AllActivityView2: View {
     @State private var searchText = ""
     
     var body: some View {
-        VStack  {
-            HStack  {
-                Button {
-                    
-                }
-            label: {
-                Image(systemName: "chevron.left")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 15, height: 15)
-                    .padding()
-                }
+        NavigationView {
+//            HStack  {
+//                Button {
+//
+//                }
+//            label: {
+//                Image(systemName: "chevron.left")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 15, height: 15)
+//                    .padding()
+//                }
+//
+//                Text ("All Activities")
+//                    .padding(.leading, 100)
+//
+//            }
+//            .frame(maxWidth: .infinity, alignment: .leading)
            
-                Text ("All Activities")
-                    .padding(.leading, 100)
-        
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-           
-            Image(systemName: "magnifyingglass.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 169, height: 190)
+//            Image(systemName: "magnifyingglass.circle.fill")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 169, height: 190)
                
             ZStack {
                        SearchBar(searchText: $searchText)
                        Text("Find Your Activity \(searchText)")
-                           .foregroundColor(.gray)
-                           .padding(.trailing, 170)
+//                           .foregroundColor(.gray)
+//                           .padding(.trailing, 170)
                 
-                
-                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                            .foregroundColor(.gray)
-                            .padding(.leading, 320)
+//                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
+//                            .foregroundColor(.gray)
+//                            .padding(.leading, 320)
                    }
-            
-            Spacer()
-            
+    
+        }
+        .padding()
+    }
+}
+
+struct AllActivityView: View {
+    @State private var searchText = ""
+    @State var activityCount: Int = 0
+//    @State private var selectedStatus: String
+    enum Status: String, CaseIterable {
+    case Upcoming, Recent, Done
+//    case Recent
+//    case Done
+    }
+   @State var selectedStatus: Status = .Upcoming
+
+    var bg = Color("Background")
+    var body: some View {
         
+        NavigationView {
+            VStack {
+                HStack{
+    //                forCountActivity(activityCount: $activityCount)
+                    Text("\(activityCount) Activities")
+                        .font(.system(size: 22, weight: .bold))
+                    Spacer()
+                    Picker("picker", selection: $selectedStatus) {
+                        ForEach(Status.allCases, id: \.self) { status in
+                            Text("\(status.rawValue)")
+                        }
+                    }
+                }
+                VStack {
+                    Image("orange-guy-all-activity")
+                    Text("Your Activity is empty")
+                        .foregroundColor(AppColor.emptyGray)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            }
+            
+        }
+        .searchable(text: $searchText, prompt: "Find your activity")
+    }
+}
+
+struct forCountActivity: View {
+    @Binding var activityCount: Int
+    
+    var body: some View {
+        Button("Tambah activity") {
+            activityCount += 1
         }
     }
+    
+    //setiap activity nambah satu di sini nambah juga
+    
+    
+}
+
+//extension Color {
+//    let backgroundCol = Color
+//}
+
+struct AppColor {
+    static let bgCol = Color("bg")
+    static let emptyGray = Color("empty-gray")
 }
 
 struct AllActivityView_Previews: PreviewProvider {
